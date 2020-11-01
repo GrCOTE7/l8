428de35b45114ebef504dd5d33efff9ad644715d
GrCOTE7 <GrCOTE7@GMail.com>
2020-11-01 12:07:39 +0100

    Init

---
+++ resources/views/article.blade.php
@@ -0,0 +1,9 @@
+@extends('template')
+
+@section('titre')
+    Les articles
+@endsection
+
+@section('contenu')
+    <p>Cest l'article n° {{ $numero }}</p>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * Ce fichier est la propriété de l8 (c) 2020
+ *
+ */
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+  return view('vue1');
+});
+
+Route::get('{n}', function ($n) {
+  return 'Je suis la page '.$n.' !';
+})->where('n', '[1-3]');
+
+Route::get('test', function () {
+  return response('un test', 200)->header('Content-Type', 'text/plain');
+});
+
+Route::get('article/{n}', function ($n) {
+  return view('article')->with('numero', $n);
+})->where('n', '[0-9]+');
+
+Route::get('facture/{n}', function ($n) {
+  return view('facture')->with('numero', $n);
+})->where('n', '[0-9]+');
