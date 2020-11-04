c00911386c13aa4a383fca00d98b3b390281ea14
GrCOTE7 <GrCOTE7@GMail.com>
2020-11-04 14:27:13 +0100

    Up

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -12,21 +12,21 @@
                         <input type="text" class="form-control  @error('nom') is-invalid @enderror" name="nom" id="nom"
                             placeholder="Votre nom" value="{{ old('nom') }}">
                         @error('nom')
-                        <div class="invalid-feedback">{{ $message }}</div>
+                            <div class="invalid-feedback">{{ $message }}</div>
                         @enderror
                     </div>
                     <div class="form-group">
                         <input type="email" class="form-control  @error('email') is-invalid @enderror" name="email"
                             id="email" placeholder="Votre email" value="{{ old('email') }}">
                         @error('email')
-                        <div class="invalid-feedback">{{ $message }}</div>
+                            <div class="invalid-feedback">{{ $message }}</div>
                         @enderror
                     </div>
                     <div class="form-group">
                         <textarea class="form-control  @error('message') is-invalid @enderror" name="message" id="message"
                             placeholder="Votre message">{{ old('message') }}</textarea>
                         @error('message')
-                        <div class="invalid-feedback">{{ $message }}</div>
+                            <div class="invalid-feedback">{{ $message }}</div>
                         @enderror
                     </div>
                     <button type="submit" class="btn btn-secondary">Envoyer !</button>
