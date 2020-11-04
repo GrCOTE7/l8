d2f988266a9a8ae9bd5dafe36ba9b2df66d34b8d
GrCOTE7 <GrCOTE7@GMail.com>
2020-11-05 00:17:17 +0100

    Up Photo

--- resources/views/photo_ok.blade.php
+++ resources/views/photo_ok.blade.php
@@ -4,7 +4,7 @@
     <br>
     <div class="container">
         <div class="row card text-white bg-dark">
-            <h4 class="card-header">Envoi d'une photo</h4>
+            <h4 class="card-header">Envoi d&apos;une photo</h4>
             <div class="card-body">
                 <p class="card-text">Merci. Votre photo à bien été reçue et enregistrée.</p>
             </div>
