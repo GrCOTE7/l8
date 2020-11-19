0b259b5577752a6b1b2b0b0d44500f77a86835b5
GrCOTE7 <GrCOTE7@GMail.com>
2020-11-19 19:17:58 +0100

    Login::class => [UserLogin::class],

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -1,7 +1,13 @@
 <?php
 
+/*
+ * Ce fichier est la propriété de l8 (c) 2020
+ */
+
 namespace App\Providers;
 
+use App\Listeners\UserLogin;
+use Illuminate\Auth\Events\Login;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -9,24 +15,24 @@ use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
-    /**
-     * The event listener mappings for the application.
-     *
-     * @var array
-     */
-    protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
-        ],
+  /**
+   * The event listener mappings for the application.
+   *
+   * @var array
+   */
+  protected $listen = [
+    Registered::class => [
+      SendEmailVerificationNotification::class,
+    ],
+    Login::class => [UserLogin::class],
     ];
 
-    /**
-     * Register any events for your application.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
+  /**
+   * Register any events for your application.
+   *
+   * @return void
+   */
+  public function boot()
+  {
+  }
 }
