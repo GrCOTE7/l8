82fcc542690c447bad020ead3cc533f88f8455df
GrCOTE7 <GrCOTE7@GMail.com>
2020-11-19 22:32:46 +0100

    Up

--- app/Http/Controllers/UController.php
+++ app/Http/Controllers/UController.php
@@ -1,12 +1,15 @@
 <?php
 
-namespace App\Http\Controllers;
+/*
+ * Ce fichier est la propriété de l8 (c) 2020
+ */
 
-use Illuminate\Http\Request;
+namespace App\Http\Controllers;
 
 class UController extends Controller
 {
-    public function index(){
-        return 'ok';
-    }
+  public function index()
+  {
+    return 'ok';
+  }
 }
