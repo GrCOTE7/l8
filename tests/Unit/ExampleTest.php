93322504f70f937d39d71a13e7b12b1b8e40fa1a
GrCOTE7 <GrCOTE7@GMail.com>
2020-12-22 15:16:04 +0100

    1er testBasicTest

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,18 +1,29 @@
 <?php
 
+/*
+ * Ce fichier est la propriété de l8 (c) 2020
+ */
+
 namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
 
-class ExampleTest extends TestCase
+/**
+ * @internal
+ * @coversNothing
+ */
+final class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
-    {
-        $this->assertTrue(true);
-    }
+  /**
+   * A basic test example.
+   *
+   * @return void
+   */
+  public function testBasicTest()
+  {
+    // $this->assertTrue(true);
+    $data   = [10, 20, 30];
+    $result = array_sum($data);
+    $this->assertSame(60, $result);
+  }
 }
