44d15181994aea5b7d7945824c5c391daabab578
GrCOTE7 <GrCOTE7@GMail.com>
2020-12-22 16:10:33 +0100

    Test avec 5 assertions

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -6,6 +6,7 @@
 
 namespace Tests\Unit;
 
+use Illuminate\Support\Str;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -22,8 +23,14 @@ final class ExampleTest extends TestCase
   public function testBasicTest()
   {
     // $this->assertTrue(true);
-    $data   = [10, 20, 30];
-    $result = array_sum($data);
-    $this->assertSame(60, $result);
+    // $data   = [10, 20, 30];
+    // $result = array_sum($data);
+    // $this->assertSame(60, $result);
+    $data = 'Je suis petit';
+    $this->assertTrue(Str::startsWith($data, 'Je'));
+    $this->assertFalse(Str::startsWith($data, 'Tu'));
+    $this->assertSame(Str::startsWith($data, 'Tu'), false);
+    $this->assertStringStartsWith('Je', $data);
+    $this->assertStringEndsWith('petit', $data);
   }
 }
