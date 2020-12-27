8c92e45888db06e6fa2595a11cf20df753b21e5a
GrCOTE7 <GrCOTE7@GMail.com>
2020-12-27 14:35:17 +0100

    Reset Fresh Laravel

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,1 @@
-window.UIkit = require("uikit");
-window.Icons = require("uikit/dist/js/uikit-icons");
-UIkit.use(window.Icons);
+require('./bootstrap');
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,18 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 /*
- * Ce fichier est la propriété de l8 (c) 2020
- */
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
-Route::middleware('web')->group(function () {
-  Route::get('/demo', static function () {
-    return view('laravel-uikit::demo');
-  })->name('demo');
-});
-
-Route::middleware('auth')->group(function () {
-  Route::prefix('profile')->group(function () {
-    Route::view('updatepassword', 'profile.updatepassword')->name('profile.updatepassword');
-    Route::view('updateinfos', 'profile.updateinfos')->name('profile.updateinfos');
-  });
+Route::get('/', function () {
+    return view('welcome');
 });
